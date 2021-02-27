b30a9ed0f85ece9755daa694c32185bf37aecb04
Gabrielius Vabalas <49102436+GVabal@users.noreply.github.com>
2021-02-27 19:24:28 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import {GamePageComponent} from './game-page/game-page.component';
 import {SentencePageComponent} from './sentence-page/sentence-page.component';
 
 const routes: Routes = [
-  { path: '/game', component: GamePageComponent, pathMatch: 'full' },
-  { path: '/sentences', component: SentencePageComponent, pathMatch: 'full' },
-  { path: '', redirectTo: '/game', pathMatch: 'full' },
+  { path: 'game', component: GamePageComponent},
+  { path: 'sentences', component: SentencePageComponent},
+  { path: '', component: GamePageComponent},
+  // { path: '', redirectTo: '/game', pathMatch: 'full' },
 ];
 
 @NgModule({
